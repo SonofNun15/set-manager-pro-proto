d8405f87c59adfb120c8860e781bcf397d05fcf7
msgraber <s.graber@sundialproperties.com>
2015-07-18 13:37:30 -0400

    Added types for firebase and angularfire.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="firebase/firebase.d.ts" />
+/// <reference path="angularfire/angularfire.d.ts" />
