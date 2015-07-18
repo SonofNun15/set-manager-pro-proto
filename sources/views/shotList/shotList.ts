8860028881e54e23a4f6f3dd3928df656f4784ba
SamGraber <sgraber@renovo1.com>
2015-07-17 21:58:49 -0400

    Added a shot list controller

--- sources/views/shotList/shotList.ts
+++ sources/views/shotList/shotList.ts
@@ -1,14 +1,31 @@
 /// <reference path='../../../typings/angularjs/angular.d.ts' />
 
 module sm.views.shotList {
+	class ShotListController {
+		scene: string;
+		storyDay: string;
+		timeToShoot: string;
+		shotDuration: string;
+		
+		showShot(): void {
+			console.log('scene: ' + this.scene);
+			console.log('storyDay: ' + this.storyDay);
+			console.log('timeToShoot: ' + this.timeToShoot);
+			console.log('shotDuration: ' + this.shotDuration);
+		}
+	}   
+	
 	function shotList(): ng.IDirective {
 		'use strict';
 		return {
 			restrict: 'E',
 			templateUrl: 'views/shotList/shotList.html',
+			controller: 'ShotListController',
+			controllerAs: 'shotList',
 		};
 	}
 	
 	angular.module('sm.views.shotList', [])
-		.directive('smShotList', shotList);
+		.directive('smShotList', shotList)
+		.controller('ShotListController', ShotListController);
 }
