8995d9ed13ea6e80009eeefb2b7e7794795a6d0e
SamGraber <sgraber@renovo1.com>
2015-07-17 20:58:36 -0400

    Created a views module

---
+++ sources/views/views.module.js
@@ -0,0 +1,9 @@
+/// <reference path='../../typings/angularjs/angular.d.ts' />
+var sm;
+(function (sm) {
+    var views;
+    (function (views) {
+        angular.module('sm.views', ['sm.views.shotList']);
+    })(views = sm.views || (sm.views = {}));
+})(sm || (sm = {}));
+//# sourceMappingURL=views.module.js.map
