19c2aade1770df8e57f39876c95d8964329eeb2c
SamGraber <sgraber@renovo1.com>
2015-07-17 21:00:13 -0400

    Added module dependencies to ui-router and sm.views

---
+++ sources/app.js
@@ -0,0 +1,10 @@
+/// <reference path='../typings/angularjs/angular.d.ts' />
+var sm;
+(function (sm) {
+    'use strict';
+    angular.module('sm', [
+        'ui.router',
+        'sm.views',
+    ]);
+})(sm || (sm = {}));
+//# sourceMappingURL=app.js.map
