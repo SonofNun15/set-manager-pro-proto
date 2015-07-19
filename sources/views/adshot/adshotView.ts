7092200d8a93be409cb0354f0a976582b4cc1f4d
msgraber <s.graber@sundialproperties.com>
2015-07-19 18:53:01 -0400

    Added schedule vie and ad shot list.

---
+++ sources/views/adshot/adshotView.ts
@@ -0,0 +1,15 @@
+/// <reference path='../../../typings/angularjs/angular.d.ts' />
+
+module sm.views.adshotView {
+	export function adshotView():ng.IDirective{
+		'use strict';
+		return {
+			restrict:'E', 
+			templateUrl: 'views/adshot/adshotView.html',
+			scope: {
+				shot: '=',
+				editShot: '&',
+			}
+		}
+	}
+}
--- sources/views/views.module.js
+++ sources/views/views.module.js
@@ -3,7 +3,6 @@ var sm;
 (function (sm) {
     var views;
     (function (views) {
-        angular.module('sm.views', ['sm.views.shotList', 'sm.views.shot']);
+        angular.module('sm.views', ['sm.views.shotList', 'sm.views.shot', 'sm.views.schedule', 'sm.views.adshot']);
     })(views = sm.views || (sm.views = {}));
 })(sm || (sm = {}));
-//# sourceMappingURL=views.module.js.map
