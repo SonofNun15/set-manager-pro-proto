ec1aa0c6965ca0a8dcc0f9ed6bef64931cd88b62
msgraber <s.graber@sundialproperties.com>
2015-07-19 22:50:28 -0400

    Corrected module name.

--- sources/views/adshot/adshot.module.ts
+++ sources/views/adshot/adshot.module.ts
@@ -1,7 +1,7 @@
 /// <reference path='../../../typings/angularjs/angular.d.ts' />
 /// <reference path='./adshotView.ts' />
 
-module sm.views.shot {
+module sm.views.adshot {
 	angular.module('sm.views.adshot', [])
 		.directive('smAdshotView', sm.views.adshotView.adshotView);
 }
