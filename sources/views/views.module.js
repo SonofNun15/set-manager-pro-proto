1fb0bba75746675f359a93e3ec4e16f7ae90d4f2
Josh Graber <jgraber@renovo1.com>
2015-07-25 13:36:07 -0400

    Remove js files
    JS files can be generated

--- sources/views/views.module.js
+++
@@ -1,8 +0,0 @@
-/// <reference path='../../typings/angularjs/angular.d.ts' />
-var sm;
-(function (sm) {
-    var views;
-    (function (views) {
-        angular.module('sm.views', ['sm.views.shotList', 'sm.views.shot', 'sm.views.schedule', 'sm.views.adshot']);
-    })(views = sm.views || (sm.views = {}));
-})(sm || (sm = {}));
