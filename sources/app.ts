72b9c7b3f731f629bb57c630e293199e9ec8e583
Josh Graber <josh.graber.85@gmail.com>
2015-07-25 18:05:35 -0400

    Add child references
    Add child references so children are rendered before their parents.

--- sources/app.ts
+++ sources/app.ts
@@ -1,5 +1,8 @@
 /// <reference path='../typings/angularjs/angular.d.ts' />
 
+/// <reference path='config/config.module.ts' />
+/// <reference path='views/views.module.ts' />
+
 module sm {
 	'use strict';
 
--- sources/views/views.module.ts
+++ sources/views/views.module.ts
@@ -1,5 +1,10 @@
 /// <reference path='../../typings/angularjs/angular.d.ts' />
 
+/// <reference path='adshot/adshot.module.ts' />
+/// <reference path='schedule/schedule.ts' />
+/// <reference path='shot/shot.module.ts' />
+/// <reference path='shotList/shotList.ts' />
+
 module sm.views {
 	angular.module('sm.views', [
 		'sm.views.shotList',
