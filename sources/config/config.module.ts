144c074d81b217576821591a18e5785434757851
Josh Graber <josh.graber.85@gmail.com>
2015-07-25 18:06:32 -0400

    Fix routing
    Fixed the routing configuration. Also added a base url reference to the
    index (required for html 5 ui routing)

--- sources/config/config.module.ts
+++ sources/config/config.module.ts
@@ -2,15 +2,23 @@
 /// <reference path='../../typings/angular-ui-router/angular-ui-router.d.ts' />
 
 module sm.config {
-	angular.module('sm.config', [])
-		.config(routeConfiguration);
-		
-	routeConfiguration.$inject = ['$state'];
-	function routeConfiguration($state: ng.ui.IStateService): void {
-		'use strict';
-	}
-	routes.$inject = ['$stateProvider'];
+	angular.module('sm.config', [
+		'ui.router',
+	]).config(routeConfiguration)
+	.config(routes);
 
+	routeConfiguration.$inject = [
+		'$locationProvider',
+		'$urlRouterProvider',
+	];
+	function routeConfiguration($locationProvider: angular.ILocationProvider,
+								$urlRouterProvider: angular.ui.IUrlRouterProvider): void {
+		'use strict';
+		$locationProvider.html5Mode(true);
+		$urlRouterProvider.otherwise('/');
+	}
+
+	routes.$inject = ['$stateProvider']; // Include $state if necessary to init
 	function routes($stateProvider: ng.ui.IStateProvider): void {
 		$stateProvider
 			.state('shotList', {
