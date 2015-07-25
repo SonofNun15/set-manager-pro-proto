3c139f06ad047513eed4fec8a340b8789ab561bb
Josh Graber <josh.graber.85@gmail.com>
2015-07-25 18:02:37 -0400

    Update the server to point to new output folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 //Lets define a port we want to listen to
 //const
-var PORT = 8080; 
+var PORT = 8080;
 
 var app = express();
 
@@ -10,13 +10,13 @@ var serverDirectory = __dirname;
 
 console.log(serverDirectory);
 
-app.use('/views', express.static(serverDirectory + '/sources/views'));
-app.use('/sources', express.static(serverDirectory + '/sources'));
-app.use('/bower_components', express.static(serverDirectory + '/bower_components'));
-app.use('/css', express.static(serverDirectory + '/css'));
+app.use('/views', express.static(serverDirectory + '/output/debug/views'));
+app.use('/sources', express.static(serverDirectory + '/output/debug'));
+app.use('/bower_components', express.static(serverDirectory + '/output/debug/bower_components'));
+app.use('/css', express.static(serverDirectory + '/output/debug/css'));
 
 app.get('/app/*', function (request, response) {
-	return response.sendFile(serverDirectory + '/sources/index.html'); 
+	return response.sendFile(serverDirectory + '/output/debug/index.html');
 });
 
 app.get('/', function(request, response) {
