3de0236367c658040ac5268193aa687e51e10d33
Josh Graber <josh.graber.85@gmail.com>
2015-08-06 16:19:54 -0400

    Move 'css' folder into 'assets' folder
    This folder will contain images and other assets as well.

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ console.log(serverDirectory);
 
 app.use('/views', express.static(serverDirectory + '/output/debug/views'));
 app.use('/sources', express.static(serverDirectory + '/output/debug'));
-app.use('/css', express.static(serverDirectory + '/output/debug/css'));
 app.use('/libraries', express.static(serverDirectory + '/output/debug/libraries'));
+app.use('/assets', express.static(serverDirectory + '/output/debug/assets'));
 
 app.get('/app/*', function (request, response) {
 	return response.sendFile(serverDirectory + '/output/debug/index.html');
