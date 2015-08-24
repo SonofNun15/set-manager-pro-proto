321764e25f2ab0f985d4055ec288a2e36f94f8d7
Josh Graber <josh.graber.85@gmail.com>
2015-08-24 07:37:55 -0400

    Add more fields to form

--- source/registration/registrationController.ts
+++ source/registration/registrationController.ts
@@ -4,12 +4,19 @@ module sm.login {
 	export var controllerName: string = 'RegistrationController';
 
 	export interface IRegistrationController {
+		referralOptions: string[];
 	}
 
 	export class RegistrationController implements IRegistrationController {
+		referralOptions: string[];
+
 		static $inject: string[] = ['$location'];
 		constructor(private $location: ng.ILocationService) {
-
+			this.referralOptions = [
+				'Suggested by friend',
+				'Google search',
+				'Other',
+			];
 		}
 	}
 }
